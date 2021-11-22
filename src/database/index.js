bcbe7183ebcd597d99fca9cbd88ba1bb0d50e8ad
Diego Almeida <dyhalmeida@gmail.com>
2021-11-22 10:55:28 -0300

    feat: Adicionado conexão de entidades e associação de relacionamentos

--- src/database/index.js
+++ src/database/index.js
@@ -3,6 +3,12 @@ const Sequelize = require('sequelize');
 /** Configuração de conexão com banco de dados */
 const databaseConfig = require('../config/database');
 
+const People = require('../models/People');
+const Account = require('../models/Account');
+const Transaction = require('../models/Transaction');
+
+const models = [People, Account, Transaction];
+
 class Database {
   constructor() {
     this.connectSQLite();
@@ -10,6 +16,10 @@ class Database {
 
   connectSQLite() {
     this.connection = new Sequelize(databaseConfig);
+    models.forEach(model => model.init(this.connection));
+    models.forEach(
+      model => model.associate && model.associate(this.connection.models),
+    );
   }
 }
 
